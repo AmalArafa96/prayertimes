4c108e488c0b14c3f8db137046b09cbd9b9b5aef
AmalArafa96 <eng.amal.arafa@gmail.com>
2026-06-03 13:16:07 +0200

    update bg

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Prayer from "../Prayer/Prayer";
 import Spinner from "../Prayer/Spinner/Spinner";
 import bgImage from "../assets/background.jpg";
+
 import "./main.css";
 
 export default function Main() {
@@ -29,15 +30,22 @@ export default function Main() {
         setDateTimes(data_prayer.data.date.gregorian.date);
         console.log("data time is", data_prayer.data.timings);
         console.log("date is", data_prayer.data.date.gregorian.date);
-        setTimeout(() => {
-          setLoading(false);
-        }, 2400);
+        
       } catch (error) {
         console.log(error);
       }
     };
     fetchPrayerTimes();
   }, [city]);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = bgImage;
+  
+    img.onload = () => {
+      setLoading(false);
+    };
+  }, []);
   
 
   function formateTimes(time){
